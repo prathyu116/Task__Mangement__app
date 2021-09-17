cebba9ef99aeb5da77930fe35f68f21bd42cb9f2
prathyu116 <prathyuprasad04669@gmail.com>
2021-09-18 02:07:47 +0530

    task management complte

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let globalStore = [];
 console.log(taskContainer);
 const newCard = ({ id, imageUrl, taskTitle, taskDescription, taskType }) =>
     `<div class="col-md-6 col-lg-4 py-3" id=${id}>
-    <div class="card ">
+    <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" id=${id} class="btn btn-outline-success" onclick= "editCard.apply(this,arguments)" ><i
     class="fas fa-pencil-alt"  id=${id} onclick= "editCard.apply(this,arguments)"></i></button>
@@ -17,22 +17,53 @@ const newCard = ({ id, imageUrl, taskTitle, taskDescription, taskType }) =>
     </div>
     <div class="img-container" ">
     <img src=${imageUrl}
-    class="card-img-top " alt="...">
+    class="card-img-top  rounded-lg" alt="...">
     </div>
    
     <div class="card-body rounded">
-        <h5 class="card-title d-flex justify-content-center border border-4">${taskTitle}</h5>
-        <p class="card-text d-flex justify-content-center text-center">${taskDescription}
+        <h5 class="card-title d-flex justify-content-start border border-4">${taskTitle}</h5>
+        <p class="card-text d-flex justify-content-start text-center">${taskDescription}
         </p>
-        <span class="badge bg-primary d-flex justify-content-center ">${taskType}</span>
+        <span class="badge bg-primary d-flex justify-content-start ">${taskType}</span>
     </div>
     <div class="card-footer text-muted  d-flex justify-content-end">
-        <button type="button"  id=${id} class="btn btn-outline-primary float-end ">Open Task</button>
-        
+        <button type="button" id=${id} onclick= "yourTask.apply(this,arguments)" class="btn btn-outline-primary float-end" data-bs-toggle="modal"  data-bs-target="#exampleModal1">Open Task
+</button>
     </div>
 </div>
 </div>
+<!-- Button trigger modal -->
 
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Your Task</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="img-container" ">
+      <img src=${imageUrl}
+      class="card-img-top  rounded-lg" alt="...">
+      </div>
+     
+      <div class="card-body rounded">
+          <h5 class="card-title d-flex justify-content-center ">${taskTitle}</h5>
+          <p class="card-text d-flex justify-content-center text-center ">${taskDescription}
+          </p>
+          <span class="badge bg-primary d-flex justify-content-center  ">${taskType}</span>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+
+
+</div>
 `;
 
 
@@ -77,6 +108,27 @@ const saveChanges = () => {
     // Add localStorage
     updateLocalStorage();
 };
+const yourTask =(event)=>{
+    event = window.event;
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+    globalStore = globalStore.map((task) => {
+        if (task.id === targetID) {
+            return {
+                id: task.id,
+                imageUrl: task.imageUrl,
+                taskTitle: task.taskTitle,
+                taskType: task.taskType,
+                taskDescription: task.taskDescription,
+            };
+            
+        }
+        return task;
+       
+    });
+
+};
+
 
 const deleteCard = (event) => {
     //  id of the card
@@ -109,6 +161,7 @@ const editCard = (event) => {
     event = window.event;
     const targetID = event.target.id;
     const tagname = event.target.tagName;
+    
 
     let parentElement;
 
@@ -177,3 +230,4 @@ const saveEditChanges = (event) => {
 };
 
 
+//searching
